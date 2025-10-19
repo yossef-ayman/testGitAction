f7ea9ca5678e4d539d1cf48e9debf7c143e0b2fa
yossef-ayman <yossefayman664@gmail.com>
2025-10-19 12:13:53 +0300

    Initial commit

---
+++ src/main/java/org/example/Mathtest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Mathtest {
+    public  int add(int a, int b) {
+        return a + b;
+    }
+    public  int sub(int a, int b) {
+        return a - b;
+    }
+    public  int mul(int a, int b) {
+        return a * b;
+    }
+    public  int div(int a, int b) {
+        return a / b;
+    }
+    public  int mod(int a, int b) {
+        return a % b;
+    }
+
+}
+
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        Mathtest mat = new Mathtest();
+        int result = mat.add(2, 3);
+        Assertions.assertEquals(5, result, "Addition should be correct");
+    }
+    @Test
+    void testSubtract() {
+        Mathtest mat = new Mathtest();
+        int r = mat.sub(5,1 );
+        Assertions.assertEquals(4, r, "Subtraction should be correct");
+    }
+    @Test
+
+    void testMultiply() {
+        Mathtest mat = new Mathtest();
+        int r= mat.mul(5,2);
+        Assertions.assertEquals(10, r, "Multiplication should be correct");
+    }
+    @Test
+
+    void testDivide() {
+        Mathtest mat = new Mathtest();
+        double r= mat.div(10,2);
+        Assertions.assertEquals(5, r, "Division should be correct");
+
+    }
+    @Test
+    void testMod() {
+        Mathtest mat = new Mathtest();
+        int r= mat.mod(5,2);
+        Assertions.assertEquals(1, r, "Mod should be correct");
+
+    }
+}
